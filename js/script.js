d4ff917a3463a20eb10a6bfeedc21a7019bb0de8
Ruben Ponce <ruben_ponce1@yahoo.com>
2018-11-05 13:23:42 -0800

    got it to add thedisable stuff

--- js/script.js
+++ js/script.js
@@ -27,29 +27,64 @@ $jobTitle.change( function(){
   });
 
 //colors must match design with jQuery. However All options in HTML must be displayed
-//Change design options on button pres.
-    $shirtDesign = $("#design");
-    prependOption();
-    $shirtDesign.change( function(){
-    $shirtOptions = $("#color option")
-    if($(this).val()==="js puns"){
-        for (let i = 0; i < $shirtOptions.length; i++) {
-            if(i>3){
-            $($shirtOptions[i]).hide();
-          }
+//Change design options on option press.
+//come back and hide the color option until press.
+$shirtDesign = $("#design");
+prependOption();
+$shirtDesign.change( function(){
+  $shirtOptions = $("#color option")
+  //selects only js pun designs
+  if($(this).val()==="js puns"){
+      for (let i = 0; i < $shirtOptions.length; i++) {
+          if(i>3){
+          $($shirtOptions[i]).hide();
+        }
+        else {
+          $($shirtOptions[i]).show();
+        }
+      }
+    }
+    //selects only heart js designs
+    if($(this).val()==="heart js"){
+      for (let i = 0; i < $shirtOptions.length; i++) {
+          if(i<=3){
+          $($shirtOptions[i]).hide();
+        }
           else {
-            $($shirtOptions[i]).show();
-          }
+          $($shirtOptions[i]).show();
         }
       }
-      if($(this).val()==="heart js"){
-        for (let i = 0; i < $shirtOptions.length; i++) {
-            if(i<=3){
-            $($shirtOptions[i]).hide();
-          }
-            else {
-            $($shirtOptions[i]).show();
-          }
-        }
-      }
-    });
+    }
+  });
+
+//disable conflicting time schedules in field "activities"
+//add up total costs later
+  /*if a checkbox is check AND it conflicts with time, {
+    disable check boxes that it disables.
+
+    else enable checkboxes
+}*/
+//gather values of time based on array of inputs.
+//$(".activities input")[i]
+  //i = 0 none;
+  //i = 1 conflicts with 3, 5
+  // i = 2 conflicts with 4,6
+
+$activity = $(".activities");
+let activityEvents = $(".activities input");
+$activity.change( function(){
+  for (let i = 0; i < activityEvents.length; i++) {
+  if($(".activities input")[i].checked&& i%2 != 0){
+    console.log('checked');
+    $(".activities input")[2*i+1].disabled = true;
+    $(".activities input")[2*i+3].disabled = true;
+    $(".activities input")[i].disabled = false;
+
+  } else if($(".activities input")[i].checked&& i%2 === 0){
+    console.log('unchecked');
+    $(".activities input")[2*i+1].disabled = false;
+    $(".activities input")[2*i+3].disabled = false;
+    $(".activities input")[i].disabled = true;
+}
+
+});
