74b557fc60a19410a8104b0b625386692648eb69
Ruben Ponce <ruben_ponce1@yahoo.com>
2018-11-06 02:52:20 -0800

    added dynamic payment that changes with respect to current payment option

--- js/script.js
+++ js/script.js
@@ -2,10 +2,8 @@ console.log('interactive form test');
 
 //if option value = "other"
   //create input field type "text" under options
+/*-----------------Job Title-----------------*/
 
-$jobTitle = $("#title");
-$jobInput = $("#input");
-$otherLabel = $("#otherLabel");
 
 function prependOption(){
   $("#color").prepend('<option selected disabled hidden>Choose a design </option>');
@@ -13,6 +11,10 @@ function prependOption(){
 
 //Selecting "other" within the field will add or remove an input field.
 //come back and make showInput();
+$jobTitle = $("#title");
+$jobInput = $("#input");
+$otherLabel = $("#otherLabel");
+
 $otherLabel.hide();
 $jobInput.hide();
 $jobTitle.change( function(){
@@ -25,10 +27,10 @@ $jobTitle.change( function(){
         $jobInput.hide();
       }
   });
-
 //colors must match design with jQuery. However All options in HTML must be displayed
 //Change design options on option press.
 //come back and hide the color option until press.
+/*-----------------Shirt Design-----------------*/
 $shirtDesign = $("#design");
 prependOption();
 $shirtDesign.change( function(){
@@ -57,22 +59,15 @@ $shirtDesign.change( function(){
     }
   });
 
-// if CHECKED && has label regex(9AM){
-//   disable any checkbox that is UNCHECKED that has regex(9AM)
-// } else if CHECKED = FALSE{
-//enable any checkbox that is UNCHECKED that has regex(9AM)
-//}
 
-  //i = 0 none;
-  //i = 1 conflicts with 3, 5
-  // i = 2 conflicts with 4,6
-
-$activity = $(".activities");
+/*-----------------Events for the conference-----------------*/
 $activityEvents = $(".activities input");
 const labelofInputAM = /Tuesday ?9[a][m]/;
 const labelofInputPM = /Tuesday ?1[p][m]/;
   let morningActivity = [];
   let dayActivity = [];
+  /*create arrays based on timing of the events that conflict by gathering
+   a conflicting event using regex and declare a class name*/
   for (let i = 0; i < $activityEvents.length; i++) {
     if(labelofInputAM.test($activityEvents.parent()[i].textContent)){
         morningActivity.push($activityEvents[i]);
@@ -83,8 +78,9 @@ const labelofInputPM = /Tuesday ?1[p][m]/;
         $activityEvents[i].className = 'dayTime';
       }
   }
-
+//makes the events  disable if they are at conflicting times
 $activityEvents.change(function(e){
+  //disables any morning event that conflicts with the selected event
   for (let i = 0; i < morningActivity.length; i++) {
   if(e.target.className==="morningTime"){
       morningActivity[i].disabled = true;
@@ -93,6 +89,7 @@ $activityEvents.change(function(e){
       morningActivity[i].disabled = false;
     }
   }
+  //diables any day time event that conflicts with the selected event
   for (let i = 0; i < dayActivity.length; i++) {
   if(e.target.className==="dayTime"){
       dayActivity[i].disabled = true;
@@ -101,12 +98,40 @@ $activityEvents.change(function(e){
       dayActivity[i].disabled = false;
     }
   }
-
-  // if($activityEvents[i].checked){
-  //   $activityEvents[i].disabled = false;
-  // } else if(e.target.checked===false){
-  //   $activityEvents[i].disabled = false;
-  // }
-
+});
+/*-----------------Payment Options-----------------*/
+$paymentOptions = $("#payment option");
+$creditCardDetails = $("#credit-card");
+$paypal = $("p")[0];
+$bitcoin = $("p")[1];
+$($paypal).hide();
+$($bitcoin).hide();
+$("#payment option")[1].selected = true;
+$("#payment").change(function(){
+  if($(this).val()!="creditCard" || $(this).val()!= "paypal" || $(this).val()!="bitcoin"){
+    $creditCardDetails.hide();
+    $($paypal).hide();
+    $($bitcoin).hide();
+  }
+  if($(this).val() ==="creditCard") {
+    $creditCardDetails.show();
+} else if($(this).val()==="paypal"){
+        $($paypal).show();
+} else if($(this).val()==="bitcoin"){
+          $($bitcoin).show();
+}
 
 });
+
+/*-----------------Validations-----------------*/
+/*add validation to the name
+Form validation
+If any of the following validation errors exist, prevent the user from submitting the form:
+Name field can't be blank.
+Email field must be a validly formatted e-mail address (you don't have to check that it's a real e-mail address, just that it's formatted like one: dave@teamtreehouse.com for example.
+User must select at least one checkbox under the "Register for Activities" section of the form.
+If the selected payment option is "Credit Card," make sure the user has supplied a Credit Card number, a Zip Code, and a 3 number CVV value before the form can be submitted.
+Credit Card field should only accept a number between 13 and 16 digits.
+The Zip Code field should accept a 5-digit number.
+The CVV should only accept a number that is exactly 3 digits long.
+*/
